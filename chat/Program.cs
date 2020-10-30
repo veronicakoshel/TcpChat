d5b3556a7fefe2bc1432ebd82115070177d8d95c
veronicakoshel <koshelveronika@mail.ru>
2020-10-30 09:51:17 +0200

    Добавьте файлы проекта.

---
+++ chat/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace chat
+{
+    class Program
+    {
+        static ServerObject server; // сервер
+        static Thread listenThread; // потока для прослушивания
+        static void Main(string[] args)
+        {
+            try
+            {
+                server = new ServerObject();
+                listenThread = new Thread(new ThreadStart(server.Listen));
+                listenThread.Start(); //старт потока
+            }
+            catch (Exception ex)
+            {
+                server.Disconnect();
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
